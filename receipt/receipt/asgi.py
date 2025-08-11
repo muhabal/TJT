9b8360902b3d1685922d53cfc3722a69183f24cd
muhabal <muhabalbako@gmail.com>
2025-08-11 10:18:45 +0100

    configuring asgi, consumers, routing and websocket

--- receipt/receipt/asgi.py
+++ receipt/receipt/asgi.py
@@ -10,10 +10,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from tjtAdmin.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'receipt.settings')
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+    URLRouter(
+      websocket_urlpatterns
+    )
+  ),
 })
---
+++ receipt/tjtAdmin/consumers.py
@@ -0,0 +1,9 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+  def connect(self):
+    self.accept()
+
+    self.send(text_data=json.dumps({'type':'connection_established',
+      'message': 'you are now connected'}))
