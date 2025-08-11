3d415e2fe7a017b13aa7a1449b97994617510c49
muhabal <muhabalbako@gmail.com>
2025-08-11 22:05:55 +0100

    create updates.js and make changes to templates

---
+++ receipt/static/js/updates.js
@@ -0,0 +1,79 @@
+// get user 
+const User = JSON.parse(document.getElementById('user').textContent)
+
+// recent updates div
+const recent_updates_div = document.querySelector(".recent-updates .updates")
+// recent_updates_div.innerHTML = ''
+
+
+// const getTimeDiff = (date)=>{
+//   let time_sent = new Date(date).getTime()
+//   let now = new Date().getTime()
+//   if (time_sent < now){
+//     // in minutes
+//     let diff = (now - time_sent)/60000
+//     if (diff < 60){
+//       return `${Math.abs(Math.round(diff))} minutes ago`
+//     } else if (diff > 60 && diff < (diff/60)/24 ){
+//       diff = diff/60
+//       return `${Math.abs(Math.round(diff))} hours ago`
+//     } else if (diff > (diff/60)/24){
+//       diff = (diff/60)/24
+//       return `${Math.abs(Math.round(diff))} days ago`
+//     }
+//   }else if(time_sent == now){
+//     return `now`
+//   }
+//   else{
+//     let diff = now - time_sent
+//     diff = diff/60000
+//     diff = Math.abs(Math.round(diff))
+//     if (diff > 1){
+//       return `${diff} minutes ago`}
+//     else{
+//       return `1 minute ago`
+//     }
+//   }
+// }
+
+// add event listener to all submit buttons
+try{
+  const buttons = document.querySelectorAll(`[data-${page}]`)
+  buttons.forEach((button)=>{
+    var action = button.dataset[page]
+    button.addEventListener('click', (e)=>{
+      e.preventDefault()
+      
+      const note = document.createElement('div')
+      note.classList.add('update')
+      note.innerHTML = `
+                <div class="profile-photo">
+                  <img style="border-radius: 50%;" src="static/icons/tjt_logo.png" alt="">
+                </div>`
+      var notification;
+      var page_name = page.slice(0, -1)
+      if (action == 'edit'){
+        notification = `
+              <div class="message">
+                <p><b>${User}</b> ${page_name} info was changed</p>
+                <small class="text-muted">10 mins ago</small>
+              </div>`
+      }else if (action == 'add'){
+        notification = `
+              <div class="message">
+                <p><b>${User}</b> new ${page_name} created</p>
+                <small class="text-muted">10 mins ago</small>
+              </div>`
+      }else if (action == 'delete'){
+        notification = `
+              <div class="message">
+                <p><b>${User}</b> ${page_name} was deleted</p>
+                <small class="text-muted">10 mins ago</small>
+              </div>`
+      }
+      note.innerHTML += notification
+      recent_updates_div.insertBefore(note,recent_updates_div.firstChild);
+      alert(action)
+    })
+  })
+}catch(err){}
